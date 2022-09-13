7e7dad800c0046ca500350e89d8f17f5628bc62b
sirananasthefifth <90382456+sirananasthefifth@users.noreply.github.com>
2022-09-13 16:44:56 +0200

    Add files via upload

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -10,27 +10,27 @@ let result;
 while (alive)
 {
 
-    console.log("Välj en operator (*, /, +, -)");
-    operator = prompt();
-    operator = operator.toLowerCase();
-    if (operator == "exit")
+    console.log("Välj en operator (*, /, +, -)"); //Fråga användaren om nummer
+    operator = prompt();    //Användarinput
+    operator = operator.toLowerCase(); //Konvertering till lowercase
+    if (operator == "exit")     //Check if exit
     {
         break;
     }
 
-    n1 = (prompt("Skriv första numret: "));
-    n1 = n1.toLowerCase();
-    if (n1 == "exit")
+    n1 = (prompt("Skriv första numret: ")); //Nummer 1
+    n1 = n1.toLowerCase();  //Till lowercase för att kolla exit
+    if (n1 == "exit")   //kolla exit
     {
         break;
     }
-    if (n1 == "result")
+    if (n1 == "result") //kolla resultat
     {
         n1 = result;
     }
-    n1 = parseInt(n1);
+    n1 = parseInt(n1);  //konvertera till int
 
-    n2 = (prompt("Skriv andra numret: "));
+    n2 = (prompt("Skriv andra numret: "));  
     n2 = n2.toLowerCase();
     if (n2 == "exit")
     {
@@ -42,6 +42,7 @@ while (alive)
     }
     n2 = parseInt(n2);
 
+    //Switch cases för miniräknaren
     switch (operator) 
     {
         case '*' :
